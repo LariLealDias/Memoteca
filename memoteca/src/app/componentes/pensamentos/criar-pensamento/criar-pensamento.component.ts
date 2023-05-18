3948db161afb6d1224d23cb1c96c57919d042bc1
Lari <larileal@hotmail.com.br>
2023-05-18 19:33:00 -0300

    first commit

---
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {
+    id : '1',
+    conteudo : 'aprendendo angular',
+    autoria : 'dev',
+    modelo : ''
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    alert();
+  }
+  cancelar(){
+    alert();
+  }
+
+}
